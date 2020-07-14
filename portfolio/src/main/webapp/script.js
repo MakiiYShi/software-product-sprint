d41939ba8b9e94a4d9fdd8e8d5679fdb21891656
MakiiYShi <yunshithu@163.com>
2020-07-14 22:12:51 +0800

    Finish w2-step2(Fetch data from servlet)

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -31,3 +31,9 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+async function getDataUsingAsyncAwait() {
+  const response = await fetch('/data');
+  const quote = await response.text();
+  document.getElementById('data-container').innerText = quote;
+}
